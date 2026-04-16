7948a0a1355f7545e57c9618d270f268ce8d3ebd
PABY655 <sanormangane655@gmail.com>
2026-04-16 13:13:37 +0000

    fix static files for production

--- bibliotheque_project/settings.py
+++ bibliotheque_project/settings.py
@@ -31,6 +31,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'bibliotheque_project.urls'
@@ -120,4 +121,7 @@ TIME_ZONE = 'Africa/Dakar'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedMan'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
